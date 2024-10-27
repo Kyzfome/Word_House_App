6127214809008e3b3dd058350d9dc3cfdef0c458
Kyzfome2 <domka_201811@gmail.com>
2024-10-27 22:36:47 +0200

    created-photos

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrl: './photo.component.scss'
 })
 export class PhotoComponent {
+  items1 = [
+    {
+      url: 'assets/images/20.png',
+    },
+    {
+      url: 'assets/images/11.png',
+    },
+    {
+      url: 'assets/images/15.png',
+    },
+    {
+      url: 'assets/images/17.png',
+    },
+    {
+      url: 'assets/images/13.png',
+    },
+    {
+      url: 'assets/images/18.png',
+    },
+  ]
 
+  items2 = [
+    {
+      url: 'assets/images/8.png',
+    },
+    {
+      url: 'assets/images/16.png',
+    },
+    {
+      url: 'assets/images/12.png',
+    },
+    {
+      url: 'assets/images/19.png',
+    },
+    {
+      url: 'assets/images/9.png',
+    },
+    {
+      url: 'assets/images/14.png',
+    },
+  ]
 }
